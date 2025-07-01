16dc41cbdbd850e355dce4e0fd9b593915c36a2d
jemora70 <jemora70@gmail.com>
2025-07-01 22:33:27 +0000

    Commit de actrualización 2025

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,6 +1,6 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
-{ pkgs, ... }: {
+{pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
 
